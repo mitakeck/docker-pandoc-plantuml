da292108903d3a83e25437840a06eb9c7cf220c6
mitakeck <mitake.ck@gmail.com>
2019-12-21 19:22:53 +0900

    ドキュメント配置ディレクトリを変更

--- script/convert-to-docx.sh
+++ script/convert-to-docx.sh
@@ -2,25 +2,21 @@ nextpagec=$(cat << EOF
 =====
 EOF
 )
-
 nextpagee=$(cat << EOF
 \`\`\`{=openxml}
 <w:p><w:pPr><w:sectPr><w:type w:val="nextPage" /></w:sectPr></w:pPr></w:p>
 \`\`\`
 EOF
 )
-
-mkdir tmp
-
-for mdfile in *.md; do
+# mkdir tmp
+for mdfile in `find docs | egrep .md$`; do
+    mkdir -p tmp/`dirname $mdfile`
+    mkdir -p dist/`dirname $mdfile`
     tmpfile=${mdfile}.md
     docfile=${mdfile%.md}.docx
     echo "$mdfile -> $docfile"
-
     # sed -e "s/$nextpagec/$nextpagee/g" $mdfile
     perl -pe "s|$nextpagec|$nextpagee|g" $mdfile > tmp/$tmpfile
-
     pandoc --reference-doc=template/template.docx tmp/$tmpfile -o dist/$docfile --filter /root/pandoc-plantuml.py
 done
-
 rm -rf tmp
--- script/convert-to-pdf.sh
+++ script/convert-to-pdf.sh
@@ -1,6 +1,17 @@
-for mdfile in *.md; do
+nextpagec=$(cat << EOF
+=====
+EOF
+)
+nextpagee=$(cat << EOF
+<hr />
+EOF
+)
+for mdfile in `find docs | egrep .md$`; do
+    mkdir -p tmp/`dirname $mdfile`
+    mkdir -p dist/`dirname $mdfile`
+    tmpfile=${mdfile}.md
     pdffile=${mdfile%.md}.pdf
     echo "$mdfile -> $pdffile"
-
-    pandoc -c template/github-markdown.css -t html5 $mdfile -o dist/$pdffile --filter /root/pandoc-plantuml.py
+    perl -pe "s|$nextpagec|$nextpagee|g" $mdfile > tmp/$tmpfile
+    pandoc -c template/github-markdown.css -t html5 tmp/$tmpfile -o dist/$pdffile --filter /root/pandoc-plantuml.py
 done
